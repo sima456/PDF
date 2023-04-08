38bb79ed5a72088821339f98b3850e128f11ae94
Ishimwe fabrice <110179513+sima456@users.noreply.github.com>
2023-04-08 21:59:58 +0200

    Create Setup.py

---
+++ Setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='pdfid',
+    version='0.2.1.1',
+    author=['Didier Stevens', 'Raphaël Vinot'],
+    maintainer_email='raphael.vinot@circl.lu',
+    url='https://github.com/Rafiot/pdfid',
+    description='PDFiD.',
+    packages=['pdfid'],
+)
